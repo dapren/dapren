77af86cb284da32dc637bbfa2a75de1f517e4a58
Aman Singh <singhaman@fb.com>
2017-09-07 18:22:46 -0700

    added

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 DAPREN_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ###############################################################################
-##################################################################### Functions 
+##################################################################### Functions
 function setup_shell_variables(){
   # Setup DAPREN_HOME in .bashrc file
   echo "[INFO] Setting \${DAPREN_HOME} to '${DAPREN_HOME}' in ${HOME}/.bashrc"
@@ -15,7 +15,7 @@ function setup_shell_variables(){
   cat ${HOME}/.bashrc \
     | egrep -v "(export DAPREN_HOME|export PYTHONPATH.*DAPREN_HOME|## Setup DAPREN variables)" \
     > ${HOME}/.bashrc_dapren_copy
-  
+
 
   echo "## Setup DAPREN variables" >> ${HOME}/.bashrc_dapren_copy
   echo "export DAPREN_HOME=\"${DAPREN_HOME}\"" >> ${HOME}/.bashrc_dapren_copy
@@ -31,15 +31,13 @@ function setup_shell_variables(){
 
 function install_required_python_packages(){
 	for app in flask pgsql yahoo-finance
-	do  
+	do
 		echo "[INFO] Running command 'pip install ${app}'"
 		pip install ${app}
-	done 
+	done
 }
 
 ###############################################################################
 ########################################################################## Main
 setup_shell_variables
 install_required_python_packages
-
-
