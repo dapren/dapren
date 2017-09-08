54777292cdaf9049e381218cbdf2b6be0681e041
Aman Singh <singhaman@fb.com>
2017-09-07 20:43:57 -0700

    added

---
+++ lib/urlops.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from constants import dapren_logger
+import constants
+import inspect
+import urllib2
+
+
+def url2str(site):
+    hdr = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'
+    }
+
+    req = urllib2.Request(site, headers=hdr)
+
+    try:
+        page = urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        print (e.fp.read())
+
+    content = page.read()
+    return content
+
+
+def test_url2str():
+    output_str_len = len(url2str('http://www.google.com'))
+    assert output_str_len > 1000
+
+
+
+# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------- MAIN
+# -----------------------------------------------------------------------------
+if __name__ == constants.str___main__:
+    # Execute all test methods. All test methods should start with string
+    # "test_"
+    for name in dir():
+        if name.startswith("test_"):
+            eval(name)()
+
+    dapren_logger.info("All tests run fine")
